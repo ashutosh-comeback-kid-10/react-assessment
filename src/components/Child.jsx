8fd5c7a49c4558c8bf9335e2bc66f23008aa5cc7
Ashutosh <asenthapa10@gmail.com>
2025-06-26 18:29:47 +0545

    I removed css that I put earlier

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Child = ({setCount}) => {
 
 
@@ -9,25 +8,11 @@ const Child = ({setCount}) => {
 
   const handlePlusClick = () => {
     setCount(prevNum => prevNum + 1)
-=======
-import React from "react"
-
-const Child = () => {
-
-  const [num, setNum] = React.useState(0)
-
-  const handleMinusClick = () => {
-    setNum(prevNum => prevNum - 1)
-  };
-
-  const handlePlusClick = () => {
-    setNum(prevNum => prevNum + 1)
->>>>>>> c8307c1b64cba249d9c7300e8569862914714ca5
   };
 
   return (
     <div>
-      <h1>{num}</h1>
+      
       <button type="button" className="btn" onClick={handleMinusClick}>
         -
       </button>
@@ -35,7 +20,7 @@ const Child = () => {
         +
       </button>
       <div className="negative">
-      {num < 0 ? <p>Why so negative?</p> : null}
+      
       </div>
     </div>
   );
