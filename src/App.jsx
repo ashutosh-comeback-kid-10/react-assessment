c8307c1b64cba249d9c7300e8569862914714ca5
Ashutosh <asenthapa10@gmail.com>
2025-06-26 14:01:04 +0545

    completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import Child from "./components/Child";
 
+
 const App = () => {
+
+
+
+
+
   return (
     <div className="App">
-      <h1>0</h1>
+      
       <Child />
-      <p>Why so negative?</p>
+      
     </div>
   );
 };
--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,20 +1,29 @@
+import React from "react"
+
 const Child = () => {
+
+  const [num, setNum] = React.useState(0)
+
   const handleMinusClick = () => {
-    /* Implement logic here */
+    setNum(prevNum => prevNum - 1)
   };
 
   const handlePlusClick = () => {
-    /* Implement logic here */
+    setNum(prevNum => prevNum + 1)
   };
 
   return (
     <div>
+      <h1>{num}</h1>
       <button type="button" className="btn" onClick={handleMinusClick}>
         -
       </button>
       <button type="button" className="btn" onClick={handlePlusClick}>
         +
       </button>
+      <div className="negative">
+      {num < 0 ? <p>Why so negative?</p> : null}
+      </div>
     </div>
   );
 };
