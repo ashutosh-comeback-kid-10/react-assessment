c2f80679e2e9c6b2cccede80a3a9e6d37971bbac
Ashutosh <asenthapa10@gmail.com>
2025-07-04 15:36:09 +0545

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import "./App.css";
 import { ShoppingListItem } from "./components/ShoppingListItem";
+import React from "react";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [inputValue, setInputValue] = React.useState("");
+
+  
+
+  function updateState(e) {
+    setInputValue(e.target.value);
+  }
+
+  function addItem() {
+    const duplicate = items.some(item => item.name.toLowerCase() === inputValue.toLowerCase())
+    if (inputValue === "" || duplicate) {
+      return alert("Error");
+    } else {
+      return (
+        setItems((prevItem) => [
+          ...prevItem,
+          { id: nanoid(), name: inputValue, checked: false },
+        ]),
+        setInputValue("")
+      );
+    }
+  }
+
+  function isChecked(id) {
+    setItems((prevItem) =>
+      prevItem.map((item) =>
+        id === item.id ? { ...item, checked: !item.checked } : item
+      )
+    );
+  }
+
+  function removeItem(id) {
+    setItems((prevItems) => prevItems.filter((item) => id !== item.id));
+  }
+
   return (
     <div className="container">
       <h1 className="mb-4">My Shopping List</h1>
@@ -11,13 +49,28 @@ function App() {
           type="text"
           placeholder="E.g. Carrots"
           className="v__input flex-1"
+          value={inputValue}
+          onChange={(e) => updateState(e)}
         />
-        <button className="v__button">Add</button>
+
+        <button
+          className="v__button"
+          onClick={addItem}
+        >
+          Add
+        </button>
       </div>
       <div className="v__list-container overflow-y-scroll">
-        {/* Map your data here: */}
-        <ShoppingListItem />
-        <ShoppingListItem />
+        {items.map((item) => (
+          <ShoppingListItem
+            key={item.id}
+            id={item.id}
+            item={item.name}
+            removeItem={() => removeItem(item.id)}
+            toggleChecked={() => isChecked(item.id)}
+            checked={item.checked}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/ShoppingListItem.jsx
+++ src/components/ShoppingListItem.jsx
@@ -1,11 +1,13 @@
 import classes from "./ShoppingListItem.module.css";
 
-export const ShoppingListItem = () => {
+export const ShoppingListItem = (props) => {
   return (
+    
     <div className="flex items-center p-2">
-      <input type="checkbox" className="mr-2" />
-      <h3 className="flex-1">Carrots</h3>
-      <button className={classes.removeButton}>x</button>
+      
+      <input type="checkbox" className="mr-2" onChange={props.toggleChecked} />
+      <h3 className="flex-1" style={{textDecoration: props.checked? "line-through" : "none"}}>{props.item}</h3>
+      <button className={classes.removeButton} onClick={props.removeItem}>x</button>
     </div>
   );
 };
